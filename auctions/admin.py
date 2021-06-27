ebeef15015c3a83d10013108f52931a12c5d6743
anshul <anshuldhakar03@gmail.com>
2021-06-27 14:51:17 +0530

    minor fixes

--- auctions/admin.py
+++ auctions/admin.py
@@ -2,17 +2,31 @@ from django.contrib import admin
 
 from .models import User, Listing, Category, Bid, Order, Comment, Watchlist
 
-# Register your models here.
-admin.site.register(User)
-admin.site.register(Listing)
-admin.site.register(Category)
-admin.site.register(Bid)
-admin.site.register(Order)
-admin.site.register(Comment)
-admin.site.register(Watchlist)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id','username','email','contact_no')
 
-# class ListingAdmin(admin.ModelAdmin):
-#     model = Listing
-#     list_display = ['user','Title','Category','Description','Image','Bid','Active','Time'] 
-#     list_editable = ['user','Title','Category','Description','Image','Bid','Active','Time'] 
-#     # list_editable = ['quantity', 'description', 'tax_rate', ] 
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ('id','user','Title','Category','Active')
+
+class CateAdmin(admin.ModelAdmin):
+    list_display = ('id','category')
+
+class BidAdmin(admin.ModelAdmin):
+    list_display = ('id','listing','bidder','bid')
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id','By','action')
+    
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('id','listing','commentor','comment')
+    
+class WatchlistAdmin(admin.ModelAdmin):
+    list_display = ('id','user')
+
+admin.site.register(User,UserAdmin)
+admin.site.register(Listing,ListingAdmin)
+admin.site.register(Category,CateAdmin)
+admin.site.register(Bid,BidAdmin)
+admin.site.register(Order,OrderAdmin)
+admin.site.register(Comment,CommentAdmin)
+admin.site.register(Watchlist,WatchlistAdmin)
